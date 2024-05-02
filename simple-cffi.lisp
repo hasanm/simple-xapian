e73a8945584fe23b508c2a54fbc7cae2f679bd0e
Mahmudul Hasan <cs.mahmud@gmail.com>
2024-05-02 17:35:48 -0600

    Minor

--- simple-cffi.lisp
+++ simple-cffi.lisp
@@ -11,9 +11,11 @@
 
 (cffi:defctype xapian-pointer :pointer)
 
+(cffi:defctype size :unsigned-int)
+
 (defclass my-xapian ()
   ((pointer :initform (cffi:foreign-alloc :char :count 1
-                                     :initial-element 0)
+                                                :initial-element 0)
             :initarg :pointer)
    (c-strings :initform '())))
 
@@ -44,6 +46,37 @@
   (end :int))
 
 
+(cffi:defcfun "easy_perform" :int
+  (handle my-xapian)
+  (str :pointer)
+  (beg :int)
+  (end :int)
+  (f   :pointer))
+
+(cffi:defcallback easy-write size ((size size))
+  (let ()
+    (handler-case
+        (progn (funcall (symbol-value '*easy-write-procedure*)
+                        size)
+               3)
+      (error () (if (zerop 3) 1 0)))))
+
+
+(defun my-perform (query)
+  (let ((handle (easy-init)))
+    (with-slots (pointer) handle
+      (with-output-to-string (contents)
+        (let ((*easy-write-procedure*
+                (lambda (string)
+                  (format t "~a TYPE : ~a~%"  string (type-of string))
+                  (write-string string contents))))
+          (declare (special *easy-write-procedure*))
+          (unwind-protect
+               (cffi:with-foreign-string (s query)
+                 (easy-perform handle s 0 10 (cffi:callback easy-write))))
+          (format t "OUTPUT : ~a~%" (get-output-stream-string contents)))))))
+
+
 (defun my-estimate (handle query)
   (unwind-protect
        (cffi:with-foreign-string (s query)
@@ -54,11 +87,11 @@
        (cffi:with-foreign-string (s query)
          (easy-search handle s beg end))))
 
-
 (defparameter *instance* (easy-init))
 (format t "~a~%" *instance*)
-(format t "Estimated : ~a ~%" (my-estimate *instance* "UPASS"))
-(my-search *instance* "UPASS" 0 5)
+;; (format t "Estimated : ~a ~%" (my-estimate *instance* "UPASS"))
+;; (my-search *instance* "UPASS" 0 5)
+(my-perform "UPASS")
 
 
 (sb-ext:exit)
--- simple.h
+++ simple.h
@@ -7,6 +7,7 @@ extern "C" {
     void* easy_init();
     int easy_estimate(void *ptr, char *str);
     int easy_search(void *ptr, char *str, int beg, int end);
+    int easy_perform(void *ptr, char *str, int beg, int end, size_t (*f) (int));
     
 
 #ifdef __cplusplus
