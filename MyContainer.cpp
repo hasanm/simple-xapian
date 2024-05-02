9a240769bd62751fb8a89e2ada20e09e38d88637
Mahmudul Hasan <cs.mahmud@gmail.com>
2024-05-02 16:16:10 -0600

    Minor

--- MyContainer.cpp
+++ MyContainer.cpp
@@ -1,36 +1,12 @@
 #include "MyContainer.h"
 using namespace std;
-using namespace cv;
 
 
 MyContainer::MyContainer() {
     
 }
 
-MyContainer::MyContainer(Mat m)
-    :mat (m)
-{
-} 
-
 MyContainer::~MyContainer() {
 }
 
-int MyContainer::load_image(char *filename, int color_mode) {
-    mat = imread(filename, color_mode);
-    return 0; 
-}
 
-int MyContainer::new_lines() {
-    lines.clear();
-    return 0;
-}
-
-int MyContainer::push_line(int x1, int x2, int y1, int y2, int r, int g, int b) {
-    Vec8i v(x1, x2, y1, y2, r, g, b, 0);
-    lines.push_back(v);
-    return 0;
-}
-
-vector<Vec8i> MyContainer::get_lines() {
-    return lines;
-}
--- simple.h
+++ simple.h
@@ -5,33 +5,6 @@
 extern "C" {
 #endif
     char* get_machine();
-    int draw_rectangle(int x, int y, int p, int q);
-    int copy_image(char *filename);
-    int paste_image(char *filename);
-
-    int my_merge();
-    int make_gray();
-    int grab_cut();
-    int experiment();
-    int canny_edge(int edgeThresh);
-    int hough_lines(int edgeThresh, double minTheta, double maxTheta);
-    int hough_lines_p(int edgeThresh, int voteThreshold, int minLineLength, int maxLineGap);
-    int hough_circles(double dp, double param1, double param2, int minRadius, int maxRadius);
-
-    int find_contours(int mSize, int edgeThresh);
-    int find_inside();
-
-    void* easy_init();
-    void* cut_image(void* container, int x, int y, int p, int q);
-    int save_image(void* container, char *filename);
-    int load_image(void* container, char *filename, int color_mode);
-    int new_lines(void* container);
-    int push_line(void* container, int x1, int x2, int y1, int y2, int r, int g, int b);
-    int draw_lines(void* container);
-    void* adaptive_threshold(void* container);
-    void* gaussian_blur(void* container, int mSize);    
-    
-
 
 #ifdef __cplusplus
 }
